df7646622b00642f7d6437a6f1b726000ed677b7
Nico Glennon <nicoglennon@icloud.com>
2021-11-24 20:07:54 -0500

    thingz

--- theme.config.js
+++ theme.config.js
@@ -1,11 +1,10 @@
 export default {
-  github: 'https://github.com/nicoglennon/crypto-primer',
-  docsRepositoryBase:
-    'https://github.com/nicoglennon/crypto-primer/blob/master',
-  titleSuffix: ' – Crypto 101',
+  github: 'https://github.com/nicoglennon/primer',
+  docsRepositoryBase: 'https://github.com/nicoglennon/primer/blob/main',
+  titleSuffix: ' – Primer',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Primer:</span>
+      <span className="mr-2 font-extrabold hidden md:inline">Primer</span>
       <span className="text-gray-600 font-normal hidden md:inline">
         Crypto 101
       </span>
@@ -17,45 +16,17 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta name="description" content="Primer: Crypto 101" />
+      <meta name="og:description" content="Primer: Crypto 101" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-icon-180x180.png"
+      <meta
+        name="twitter:image"
+        content="https://primer.vercel.app/intro.png"
       />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="192x192"
-        href="/android-icon-192x192.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="96x96"
-        href="/favicon-96x96.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-      />
-      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+      <meta name="twitter:site:domain" content="primer.vercel.app" />
+      <meta name="twitter:url" content="https://primer.vercel.app" />
+      <meta name="og:title" content="Primer: Crypto 101" />
+      <meta name="og:image" content="https://primer.vercel.app/intro.png" />
     </>
   ),
   search: false,
@@ -72,5 +43,5 @@ export default {
       in 🗽 © {new Date().getFullYear()}
     </>
   ),
-  unstable_faviconGlyph: '🏁',
+  unstable_faviconGlyph: '👽',
 }
